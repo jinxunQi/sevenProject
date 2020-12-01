b409467d61a50d50d06d9a16e6f03d86dd950950
574765035 <jinxun@gzitech.com>
2020-12-01 23:27:47 +0800

    11-12 页面跳转v1

--- protoss/pages/home/home.js
+++ protoss/pages/home/home.js
@@ -42,56 +42,15 @@ Page({
     });
   },
 
+   // 跳转商品详情
+   onProductsItemTap: function (event) {
+    var id = null;
+    wx.navigateTo({
+      url: '../product/product?id=' + id,
+    })
+  }
+
   // callBack: function(res){
   //   console.log(res);//上面使用闭包来代替回调
   // },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
